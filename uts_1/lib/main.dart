abc0edcb0ff9447f9ab6d0db0166324f3c69e7f5
khafid33 <alkhafidshaif@gmail.com>
2024-05-04 14:44:17 +0700

    appbar

--- uts_1/lib/main.dart
+++ uts_1/lib/main.dart
@@ -78,13 +78,41 @@ class _MyHomePageState extends State<MyHomePage> {
     // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Row(
+          children: [
+            IconButton(
+              icon: Icon(Icons.search),
+              onPressed: () {},
+            ),
+            Expanded(
+              child: Container(
+                height: 40,
+                margin: EdgeInsets.only(left: 8, right: 16),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                child: TextField(
+                  decoration: InputDecoration(
+                    hintText: 'Search',
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20.0),
+                        borderSide: BorderSide(color: Colors.blue)),
+                    filled: true,
+                    fillColor: Colors.grey[200], // warna latar belakang
+                    contentPadding: EdgeInsets.symmetric(horizontal: 16),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.qr_code),
+            onPressed: () {},
+          ),
+        ],
       ),
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
